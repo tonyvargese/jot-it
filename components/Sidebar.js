bf7bb68359c95da1ad9417bb17b2193b2d9c6eb6
Tony <tonyvar666@gmail.com>
2024-03-02 21:51:09 +0530

    added note titles

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,8 @@
 import React from "react"
 
 export default function Sidebar(props) {
+    
+    
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
             <div
@@ -10,7 +12,7 @@ export default function Sidebar(props) {
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+                <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
             </div>
         </div>
     ))
