b1669bce1dfcc46165867ccede573a68e135cdfe
Tony <tonyvar666@gmail.com>
2024-03-03 10:21:55 +0530

    removed data.js

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import React from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
-import { data } from "./data"
 import Split from "react-split"
-import {nanoid} from "nanoid"
+import { nanoid } from "nanoid"
 
 export default function App() {
     const [notes, setNotes] = React.useState(
@@ -12,11 +11,11 @@ export default function App() {
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
-    
+
     React.useEffect(() => {
         localStorage.setItem("notes", JSON.stringify(notes))
     }, [notes])
-    
+
     function createNewNote() {
         const newNote = {
             id: nanoid(),
@@ -25,14 +24,14 @@ export default function App() {
         setNotes(prevNotes => [newNote, ...prevNotes])
         setCurrentNoteId(newNote.id)
     }
-    
+
     function updateNote(text) {
-        // Put the most recently-modified note at the top
         setNotes(oldNotes => {
             const newArray = []
-            for(let i = 0; i < oldNotes.length; i++) {
+            for (let i = 0; i < oldNotes.length; i++) {
                 const oldNote = oldNotes[i]
-                if(oldNote.id === currentNoteId) {
+                if (oldNote.id === currentNoteId) {
+                    // Put the most recently-modified note at the top
                     newArray.unshift({ ...oldNote, body: text })
                 } else {
                     newArray.push(oldNote)
@@ -41,56 +40,56 @@ export default function App() {
             return newArray
         })
     }
-    
+
     function deleteNote(event, noteId) {
         event.stopPropagation()
         setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
     }
-    
+
     function findCurrentNote() {
         return notes.find(note => {
             return note.id === currentNoteId
         }) || notes[0]
     }
-    
+
     return (
         <main>
-        {
-            notes.length > 0 
-            ?
-            <Split 
-                sizes={[30, 70]} 
-                direction="horizontal" 
-                className="split"
-            >
-                <Sidebar
-                    notes={notes}
-                    currentNote={findCurrentNote()}
-                    setCurrentNoteId={setCurrentNoteId}
-                    newNote={createNewNote}
-                    deleteNote={deleteNote}
-                />
-                {
-                    currentNoteId && 
-                    notes.length > 0 &&
-                    <Editor 
-                        currentNote={findCurrentNote()} 
-                        updateNote={updateNote} 
-                    />
-                }
-            </Split>
-            :
-            <div className="no-notes">
-                <h1>You have no notes</h1>
-                <button 
-                    className="first-note" 
-                    onClick={createNewNote}
-                >
-                    Create one now
+            {
+                notes.length > 0
+                    ?
+                    <Split
+                        sizes={[30, 70]}
+                        direction="horizontal"
+                        className="split"
+                    >
+                        <Sidebar
+                            notes={notes}
+                            currentNote={findCurrentNote()}
+                            setCurrentNoteId={setCurrentNoteId}
+                            newNote={createNewNote}
+                            deleteNote={deleteNote}
+                        />
+                        {
+                            currentNoteId &&
+                            notes.length > 0 &&
+                            <Editor
+                                currentNote={findCurrentNote()}
+                                updateNote={updateNote}
+                            />
+                        }
+                    </Split>
+                    :
+                    <div className="no-notes">
+                        <h1>You have no notes</h1>
+                        <button
+                            className="first-note"
+                            onClick={createNewNote}
+                        >
+                            Create one now
                 </button>
-            </div>
-            
-        }
+                    </div>
+
+            }
         </main>
     )
 }
