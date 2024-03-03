2142274f32dc58d6433ccb59acdabca5a2b25db4
Tony <tonyvar666@gmail.com>
2024-03-03 16:47:10 +0530

    minor import error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	plugins: [react()]	
-})
+  plugins: [react()],
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
+  build: {
+    target: 'esnext',
+    polyfillDynamicImport: false,
+    jsx: 'react',
+  },
+});
